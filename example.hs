d028445b990951f9b2e32430232a8755153f7e1d
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2023-06-14 08:59:10 +0100

    feat: first example

---
+++ example.hs
@@ -0,0 +1,11 @@
+myPlus a b = a + b
+
+sq b = b * b
+
+isPositive x = if x >= 0
+                then "positive"
+                else "negative"
+
+isPeter name = if name == "peter"
+                then "Peter"
+                else "Someone else"
